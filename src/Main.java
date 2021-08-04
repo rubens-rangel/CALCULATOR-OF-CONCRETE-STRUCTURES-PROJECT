70d156b80b42e65e007290883206879819c51f6b
Rubens Rangel <rubens_rangel@hotmail.com>
2021-08-04 19:51:47 -0300

    Finalização

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
         int a = 99;
         Scanner leitor = new Scanner(System.in);  
         
- for ( int i = 0;  a != 165411/*numero aleatorio 0*/ ; i++ ) {  
+ for ( int i = 0;  a != 165411/*numero aleatorio diferente de 0,1,2,3 */ ; i++ ) {
      System.out.println("Escreva a estrutura desejada:");
      System.out.println("digite 1 para Baldrame");
      System.out.println("digite 2 para Pilar");
@@ -50,7 +50,7 @@ public class Main {
                 baldrame.altura = scan.nextDouble();
                 System.out.println("escreva o comprimento (metros)");
                 baldrame.comprimento = scan.nextDouble();
-                System.out.println("Consumo de Concreto em m(cubicos)");
+                System.out.println("Consumo de Concreto em m(cubicos):");
                 
                 //Calculo e Saidas
                 System.out.println(baldrame.calculo());                
@@ -59,12 +59,12 @@ public class Main {
                 
               //Somatorio do valor das estuturas
                 ValorTotal = ValorTotal + preco;              
-                break; // fim baldrame               
+                break; // fim Baldrame
                 
                 
                 
 
- /*           case 2: // Pilar
+            case 2: // Pilar
                 System.out.println("voce escolheu Pilar");
                 System.out.println("Escolha a Resistencia do Concreto: \n 1 - Fck 15 Mpa \n 2 - Fck 18 Mpa \n 3 - Fck 20 Mpa \n 4 - Fck 25 Mpa");
                 Scanner leitorfckp = new Scanner(System.in);
@@ -87,58 +87,69 @@ public class Main {
                         break;
                 } //Valor de cada fck
 
-                Pilar pilar = new Pilar();
+                    Pilar pilar = new Pilar();
 
-                //Entrada dos Dados
-                Scanner scanp = new Scanner(System.in);
-                System.out.println("escreva a largura: (cm)");
-                pilar.largura = scanp.nextDouble();
-                System.out.println("escreva a altura: (cm)");
-                pilar.altura = scanp.nextDouble();
-                System.out.println("escreva o comprimento (metros)");
-                pilar.comprimento = scanp.nextDouble();
-                System.out.println("Consumo de Concreto em m(cubicos)");
-                System.out.println(pilar.calculo());
-                preco = pilar.calculo() * valorfck;
-                System.out.println("Valor da Estrutura R$ "  + preco);
-                break;
-            case 3: // Calcada
-                System.out.println("voce escolheu Calcada");
-                System.out.println("Escolha a Resistencia do Concreto: \n 1 - Fck 15 Mpa \n 2 - Fck 18 Mpa \n 3 - Fck 20 Mpa \n 4 - Fck 25 Mpa");
-                Scanner leitorfckc = new Scanner(System.in);
-                int c = leitorfckc.nextInt();
-                switch (c) {
-                    case 1:
-                        valorfck = 454.78;
-                        break;
-                    case 2:
-                        valorfck = 471.61;
-                        break;
-                    case 3:
-                        valorfck = 543.02;
-                        break;
-                    case 4:
-                        valorfck = 578.87;
-                        break;
-                    default:
-                        System.out.println("opcao invalida");
-                        break;
-                } //Valor de cada fck
+                    //Entrada dos Dados
+                    Scanner scanp = new Scanner(System.in);
+                    System.out.println("escreva a largura: (cm)");
+                    pilar.largura = scanp.nextDouble();
+                    System.out.println("escreva a altura: (cm)");
+                    pilar.altura = scanp.nextDouble();
+                    System.out.println("escreva o comprimento (metros)");
+                    pilar.comprimento = scanp.nextDouble();
+                    System.out.println("Consumo de Concreto em m(cubicos):");
 
-                Calcada calcada = new Calcada();
+                    //Calculo e Saidas
+                    System.out.println(pilar.calculo());
+                    preco = pilar.calculo() * valorfck;
+                    System.out.println("Valor da Estrutura R$ " + preco + "\n");
 
-                //Entrada dos Dados
-                Scanner scanc = new Scanner(System.in);
-                System.out.println("escreva a area: m(quadrados)");
-                calcada.area = scanc.nextDouble();
-                System.out.println("escreva a altura: (cm)");
-                calcada.altura = scanc.nextDouble();
-                System.out.println("Consumo de Concreto em m(cubicos)");
-                System.out.println(calcada.calculo());
-                preco = calcada.calculo() * valorfck;
-                System.out.println("Valor da Estrutura R$ "  + preco);
-                break;
-*/
+                    //Somatorio do valor das estuturas
+                    ValorTotal = ValorTotal + preco;
+                    break; // fim Pilar
+
+                case 3: // Calcada
+                    System.out.println("voce escolheu Calcada");
+                    System.out.println("Escolha a Resistencia do Concreto: \n 1 - Fck 15 Mpa \n 2 - Fck 18 Mpa \n 3 - Fck 20 Mpa \n 4 - Fck 25 Mpa");
+                    Scanner leitorfckc = new Scanner(System.in);
+                    int c = leitorfckc.nextInt();
+                    switch (c) {
+                        case 1:
+                            valorfck = 454.78;
+                            break;
+                        case 2:
+                            valorfck = 471.61;
+                            break;
+                        case 3:
+                            valorfck = 543.02;
+                            break;
+                        case 4:
+                            valorfck = 578.87;
+                            break;
+                        default:
+                            System.out.println("opcao invalida");
+                            break;
+                    }
+
+                    Calcada calcada = new Calcada();
+
+                    //Entrada dos Dados
+                    Scanner scanc = new Scanner(System.in);
+                    System.out.println("escreva a area m(quadrados)");
+                    calcada.area = scanc.nextDouble();
+                    System.out.println("escreva a altura: (cm)");
+                    calcada.altura = scanc.nextDouble();
+                    System.out.println("Consumo de Concreto em m(cubicos):");
+
+                    //Calculo e Saidas
+                    System.out.println(calcada.calculo());
+                    preco = calcada.calculo() * valorfck;
+                    System.out.println("Valor da Estrutura R$ " + preco + "\n");
+
+                    //Somatorio do valor das estuturas
+                    ValorTotal = ValorTotal + preco;
+                    break; // fim Calcada
+
                 case 0: // Finalizar + Calculo do Total
                     System.out.println("Valor Total das Estruturas:");
                     System.out.println(ValorTotal);
